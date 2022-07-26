626d67e8353416d62f8af78c56be63e9caec3122
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2022-07-26 16:56:03 +0430

    Output class optimized

--- Phase03/project/Output.cs
+++ Phase03/project/Output.cs
@@ -4,9 +4,9 @@ public class Output
 {
     public void printTopStudents(List<Student> students)
     {
+        var sortedStudents = students.OrderByDescending(x => x.AverageGrade);
         for (var i = 0; i < 3; i++)
         {
-            var sortedStudents = students.OrderByDescending(x => x.AverageGrade);
             var student = sortedStudents.ElementAt(i);
             var name = student.FirstName + " " + student.LastName;
             var averageScore = student.AverageGrade;
