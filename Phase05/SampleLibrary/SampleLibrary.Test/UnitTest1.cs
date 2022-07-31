5cfd3b5a8c55b41c67c5890c4019a123e614e714
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2022-07-31 10:52:14 +0430

    Add InvertedIndex and FileReader classes

--- Phase05/SampleLibrary/SampleLibrary.Test/UnitTest1.cs
+++ Phase05/SampleLibrary/SampleLibrary.Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+
 namespace SampleLibrary.Test;
 
 public class UnitTest1
@@ -9,5 +10,45 @@ public class UnitTest1
     {
         var search = new SearchInDocs(necessaries, optionals, avoids);
         search.Should().Be("");
+    }
+
+    [Fact]
+    public void FileReaderTest()
+    {
+        var filesDir = @"C:\Users\alibn\RiderProjects\Summer1401-SE-Team06\Phase05\SampleLibrary\EnglishData";
+        var fileReader = new FileReader(filesDir);
+        var files = fileReader.readFiles();
+
+        var firstFileData =
+            ">This wouldn't happen to be the same thing as chiggers, " +
+            "would it>A truly awful parasitic affliction, as I understand it." +
+            "  Tiny bug>dig deeply into the skin, burying themselves.  Yuck! " +
+            " They have thes>things in OklahomaClose. My mother comes from Gainesville" +
+            " Tex, right across the borderThey claim to be the chigger capitol of the world," +
+            " and I believe themWhen I grew up in Fort Worth it was bad enough, but in " +
+            "Gainesvillin the summer an attack was guaranteedDoug McDonal";
+
+        files["58043"].Should().Be(firstFileData.ToUpper());
+    }
+
+    [Fact]
+    public void InvertedIndexMakerTest()
+    {
+        InvertedIndex invertedIndex = new InvertedIndex();
+
+        invertedIndex.indexDocument("hello ali", "1");
+        invertedIndex.indexDocument("this is a code", "2");
+        invertedIndex.indexDocument("to search some words","3");
+        invertedIndex.indexDocument("in a text, ali", "4");
+
+        var map = invertedIndex.Map;
+
+        map.ContainsKey("hello").Should().Be(true);
+        map.ContainsKey("mohammad").Should().NotBe(true);
+        
+        map["ali"].Contains("1").Should().Be(true);
+        map["ali"].Contains("4").Should().Be(true);
+        
+        map["ali"].Contains("2").Should().Be(false);
     }
 }
---
+++ Phase05/SampleLibrary/SampleLibrary/InvertedIndex.cs
@@ -0,0 +1,39 @@
+namespace SampleLibrary;
+
+public class InvertedIndex
+{
+    public void indexDocument(string document, string docId)
+    {
+        string[] words;
+
+        words = document.Split(" ");
+
+        foreach (var word in words)
+        {
+            if (word.Length > 1)
+            {
+                AddWordToMap(word, docId);
+            }
+        }
+    }
+
+    private void AddWordToMap(string word, string docId)
+    {
+        bool wordExists = Map.ContainsKey(word);
+        HashSet<string> docIdSet;
+
+        if (wordExists)
+        {
+            docIdSet = Map[word];
+        }
+        else
+        {
+            docIdSet = new HashSet<string>();
+        }
+
+        docIdSet.Add(docId);
+        Map[word] = docIdSet;
+    }
+
+    public Dictionary<string, HashSet<string>> Map { get; } = new Dictionary<string, HashSet<string>>();
+}
