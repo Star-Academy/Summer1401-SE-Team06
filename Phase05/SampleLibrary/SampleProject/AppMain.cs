ae58a3766e159e26adfa77679cf8b2fd4ac06176
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2022-08-01 11:28:56 +0430

    Stop run program

--- Phase05/SampleLibrary/SampleProject/AppMain.cs
+++ Phase05/SampleLibrary/SampleProject/AppMain.cs
@@ -6,9 +6,9 @@ public class AppMain
 {
     public static void Main(string[] args)
     {
-        var filesDir = @"C:\Users\User\Desktop\EnglishData";
-        var fileReader = new FileReader(filesDir);
-        new RunProgram().Run(fileReader);
+        // var filesDir = @"C:\Users\User\Desktop\EnglishData";
+        // var fileReader = new FileReader(filesDir);
+        // new RunProgram().Run(fileReader);
     }
     
 }
