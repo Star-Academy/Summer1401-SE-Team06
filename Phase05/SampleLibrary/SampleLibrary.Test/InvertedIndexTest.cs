3ba21b844512dfa45d914188b3b797214b42b976
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2022-08-09 10:37:38 +0430

    Resolved complicated test

--- Phase05/SampleLibrary/SampleLibrary.Test/InvertedIndexTest.cs
+++ Phase05/SampleLibrary/SampleLibrary.Test/InvertedIndexTest.cs
@@ -4,29 +4,60 @@ namespace SampleLibrary.Test;
 
 public class InvertedIndexTest
 {
-    [Fact]
-    public void InvertedIndex_InsertSomeWords_ReturnsTrue()
+    private InvertedIndex _invertedIndex;
+    public InvertedIndexTest()
     {
-        var invertedIndex = new InvertedIndex();
+        InitInvertedIndex();
+    }
 
-        invertedIndex.indexDocument("hello ali", "1");
-        invertedIndex.indexDocument("this is a code", "2");
-        invertedIndex.indexDocument("to search some words", "3");
-        invertedIndex.indexDocument("in a text, ali", "4");
-
-        var map = invertedIndex.Map;
-
-        map.ContainsKey("hello").Should().Be(true);
-        map.ContainsKey("mohammad").Should().NotBe(true);
-
-        map["ali"].Contains("1").Should().Be(true);
-        map["ali"].Contains("4").Should().Be(true);
-
-        map["ali"].Contains("2").Should().Be(false);
+    private void InitInvertedIndex()
+    {
+        _invertedIndex = new();
+        _invertedIndex.indexDocument("hello ali", "1");
+        _invertedIndex.indexDocument("this is a code", "2");
+        _invertedIndex.indexDocument("to search some words", "3");
+        _invertedIndex.indexDocument("in a text, ali", "4");
     }
 
     [Fact]
-    public void InvertedIndexMaker_InsetSomeWordsInInvertedIndex_ReturnsTrue()
+    public void InvertedIndex_ExistingWords_ReturnsTrue()
+    {
+        var map = _invertedIndex.Map;
+        
+        map.ContainsKey("hello").Should().Be(true);
+        map.ContainsKey("ali").Should().Be(true);
+    }
+    
+    
+    [Fact]
+    public void InvertedIndex_NotExistingWords_ReturnsFalse()
+    {
+        var map = _invertedIndex.Map;
+
+        map.ContainsKey("mohammad").Should().NotBe(true);
+        map.ContainsKey("test").Should().NotBe(true);
+    }
+    
+    [Fact]
+    public void InvertedIndex_DocsContainingAWord_ReturnsTrue()
+    {
+        var map = _invertedIndex.Map;
+        
+        map["ali"].Should().Contain("1");
+        map["ali"].Should().Contain("4");
+    }
+    
+    [Fact]
+    public void InvertedIndex_DocsNotContainingAWord_ReturnsTrue()
+    {
+        var map = _invertedIndex.Map;
+        
+        map["ali"].Should().NotContain("2");
+        map["ali"].Should().NotContain("3");
+    }
+
+    [Fact]
+    public void InvertedIndexMaker_InsertSomeWordsInInvertedIndex_ReturnsTrue()
     {
         var documents = new Dictionary<string, string>();
         documents["1"] = "hello ali";
