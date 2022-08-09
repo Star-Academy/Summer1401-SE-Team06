1c0853b0ff4b2cb729ad380e9aecf9c7767cccc9
matinmoradi110 <matinmoradi110@gmail.com>
2022-08-09 09:55:40 +0430

    kill temp variable

--- Phase03/project/FileReader.cs
+++ Phase03/project/FileReader.cs
@@ -6,7 +6,6 @@ public class FileReader
 {
     public string ReadFileFromDisk(string filePath)
     {
-        string text = File.ReadAllText(filePath);  
-        return text;
+        return File.ReadAllText(filePath);  
     }
 }
