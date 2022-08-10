6ca472f8544671cf28c0f19ea2a446d17e889769
matinmoradi80 <62210384+matinmoradi80@users.noreply.github.com>
2022-08-10 17:05:23 +0430

    Update Student.cs
    delete the comment

--- Phase03/project/Student.cs
+++ Phase03/project/Student.cs
@@ -3,8 +3,6 @@
 public record Student
 {
     public int StudentNumber { get; init; }
-
-    // private double averageGrade; 
     public string FirstName { get; init; }
     public string LastName { get; init; }
     public List<Lesson> Lessons { get; } = new();
