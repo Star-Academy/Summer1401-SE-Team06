0b406991c62189bc63868f35e4353dd49c512a24
Ali Bonaftizadeh <79264950+ali-bonaftizadeh@users.noreply.github.com>
2022-08-20 14:14:59 +0430

    Phase09 (#7)
    * phase05 template project added
    
    * add .idea directory to git ignore
    
    * add obj and bin directories to gitignore
    
    * first test
    
    * Add InvertedIndex and FileReader classes
    
    * Add FileReader and InvertedIndex classes
    
    * Add InvertedIndexMaker class
    
    * Delete additional directories
    
    * Add SearchInDocs class
    
    * categorize test
    
    * 'InvertedIndexMaker' class now cleaner
    
    * Add Pipeline for phase05 project
    
    * Revert "Add Pipeline for phase05 project"
    
    This reverts commit 3da134ebcf88bfbcd31da3390634f8e11ae03efd.
    
    * IO and working with file is added
    
    * Add Pipeline
    
    * Update buildPipeline.yml
    
    * Update Pipeline
    
    * Delete github/workflows directory
    
    * add pipeline
    
    * Update buildPipeline.yml
    
    * Update buildPipeline.yml
    
    * Update buildPipeline.yml
    
    * dummy commit
    
    * Stop run program
    
    * stop running program
    
    * Delete .github/workflows directory
    
    * Update README.md
    
    * Update README.md
    
    * remove run files
    
    * Delete phase06 directory
    
    * Resolved duplication and used LINQ
    
    * Resolved complicated test
    
    * Remove additional test
    
    * Resolved private field name convention and cleanup
    
    * Resolved SearchEngineTest and cleanup
    
    * empty phase08 project
    
    * Revert "empty phase08 project"
    
    This reverts commit 671790c47ba4d4cd6c8f7b46a565675db30de13d.
    
    * Add phase08 project
    
    * Delete phase08/.idea/.idea.phase08.dir/.idea directory
    
    * Delete phase08/StudentGrades/StudentGrades/obj directory
    
    * Delete phase08/StudentGrades/.idea/.idea.StudentGrades/.idea directory
    
    * Add gitignore
    
    * Add project
    
    * Delete phase08/StudentGrades/.idea/.idea.StudentGrades/.idea directory
    
    * initial commit
    
    * phase08 first
    
    * migration created
    
    * add records and average calculation
    
    * update average field in Db
    
    * Fix a bug
    
    * Update gitignore
    
    * Refactor
    
    * Resolved name convention
    
    * Resolved O/C principle for dataParser
    
    * Remove extra lines
    
    * Resolved SRP for Output printer
    
    * Resolved Extract method
    
    * phase09 added
    
    * working with exceptions!
    
    * some refactor
    
    * Some refactor
    
    * Cleanup controller class
    
    * Complete Process method
    
    * dependency injection added
    
    * InputProcessor in dependency injection
    
    * Delete Phase03 directory
    
    * remove extra folders in phase09
    
    Co-authored-by: matinmoradi110 <matinmoradi110@gmail.com>

---
+++ phase09/Program.cs
@@ -0,0 +1,29 @@
+using DefaultNamespace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IInputProcessorService, InputProcessorService>();
+builder.Services.AddSingleton<IInvertedIndexSearchService, InvertedIndexSearchService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
